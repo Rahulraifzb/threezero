296179bebed5aff3b7cedad69799a96c108841c7
Rahul rai <rrai06125@gmail.com>
2021-09-09 14:25:25 +0530

    url change in allowed host

--- core/settings.py
+++ core/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g$j1%q-vin3@ee$9kw+&ohe$v0uw=)aj3$3_yr(q^uq9r67$$f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['myquizappfzb.herokuapp.com',"127.0.0.1"]
+ALLOWED_HOSTS = ['threezero.herokuapp.com',"127.0.0.1"]
 
 
 # Application definition
